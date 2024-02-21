b8bf6a11ef76e3b42672c53b82c68f748d0a04f7
And-r648 <153227472+And-r648@users.noreply.github.com>
2024-02-21 20:48:57 +0200

    Add files via upload

---
+++ home-work.lesson4.js
@@ -0,0 +1,41 @@
+// function checkAge(age) {
+//   return age > 18 ? true : confirm("Батьки дозволили?");
+// }
+
+// let age = 17;
+
+// console.log(checkAge(age));
+
+// --------------------------------------------------------
+
+// function min(a, b) {
+//   if (a < b) {
+//     return a;
+//   } else return b;
+// }
+// // console.log(min(5, 8));
+
+// // або -----------------
+
+// let result = min(9, 4);
+// console.log(result);
+
+// -------------------------------------------------------------
+
+// const ask = (question, yes, no) => {
+//   if (confirm(question)) {
+//     yes();
+//   } else {
+//     no();
+//   }
+// };
+
+// ask(
+//   "Ви згодні?",
+//   () => {
+//     alert("Ви погодились.");
+//   },
+//   () => {
+//     alert("Ви скасували виконання.");
+//   }
+// );
