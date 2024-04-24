31c353096e3a1d080fb38184ce25fb731db4a914
And-r648 <153227472+And-r648@users.noreply.github.com>
2024-04-24 19:50:51 +0300

    Add files via upload

---
+++ home-work-dz6.js
@@ -0,0 +1,28 @@
+// ====== Завдання ======
+
+// class Calculator {
+//   constructor(options) {
+//     this.val1 = 0;
+//     this.val2 = 0;
+//   }
+
+//   read() {
+//     this.val1 = +prompt("Введіть перше значення");
+//     this.val2 = +prompt("Введыть друге значення");
+//   }
+
+//   sum() {
+//     return this.val1 + this.val2;
+//   }
+
+//   mul() {
+//     return this.val1 * this.val2;
+//   }
+// }
+
+// const calculator1 = new Calculator();
+
+// calculator1.read();
+
+// console.log(calculator1.sum());
+// console.log(calculator1.mul());
