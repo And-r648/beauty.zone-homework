f2331fd7cd4baa3cc76b8251cdf75b6b79d04013
And-r648 <153227472+And-r648@users.noreply.github.com>
2024-04-24 17:20:39 +0300

    Update home-work-dz5.js

--- home-work-dz5.js
+++ home-work-dz5.js
@@ -6,12 +6,12 @@
 //     this.phoneNumber = phoneNumber;
 //   }
 
-//   setValues(name, phoneNumber) {
+//   setDetails(name, phoneNumber) {
 //     this.name = name;
 //     this.phoneNumber = phoneNumber;
 //   }
 
-//   getValues() {
+//   getDetails() {
 //     return `${this.name}: ${this.phoneNumber}`;
 //   }
 // }
@@ -20,11 +20,11 @@
 // const abonent2 = new Abonent("Клава", "+380632236789");
 // const abonent3 = new Abonent("Вася", "+380509876643");
 
-// console.log(abonent1.getValues());
-// console.log(abonent2.getValues());
-// console.log(abonent3.getValues());
+// console.log(abonent1.getDetails());
+// console.log(abonent2.getDetails());
+// console.log(abonent3.getDetails());
 
 // ===========================================
 
-// abonent1.setValues("Maradona", "0663459877");
-// console.log(abonent1.getValues());
+// abonent1.setDetails("Maradona", "0663459877");
+// console.log(abonent1.getDetails());
