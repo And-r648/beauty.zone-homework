ce911165a1cc285d0b9073ec97e788262577534c
And-r648 <153227472+And-r648@users.noreply.github.com>
2024-04-24 16:25:29 +0300

    Add files via upload

---
+++ home-work-dz5.js
@@ -0,0 +1,30 @@
+// ==== Завдання =====
+
+// class Abonent {
+//   constructor(name, phoneNumber) {
+//     this.name = name;
+//     this.phoneNumber = phoneNumber;
+//   }
+
+//   setValues(name, phoneNumber) {
+//     this.name = name;
+//     this.phoneNumber = phoneNumber;
+//   }
+
+//   getValues() {
+//     return `${this.name}: ${this.phoneNumber}`;
+//   }
+// }
+
+// const abonent1 = new Abonent("Юля", "+380661234455");
+// const abonent2 = new Abonent("Клава", "+380632236789");
+// const abonent3 = new Abonent("Вася", "+380509876643");
+
+// console.log(abonent1.getValues());
+// console.log(abonent2.getValues());
+// console.log(abonent3.getValues());
+
+// ===========================================
+
+// abonent1.setValues("Maradona", "0663459877");
+// console.log(abonent1.getValues());
